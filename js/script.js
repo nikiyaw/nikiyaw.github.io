24a3896911726e2145c7b227138b39a09c294456
Niki Yaw <ql231995@student.ubc.ca>
2025-08-31 11:28:13 -0700

    added the button and js related script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Get the button and the body element
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+// Add an event listener for clicks on the button
+themeToggle.addEvenet<IntersectionObserverEntry('click', function() {
+    // Toggle the 'light-mode' class on the body
+    body.classList.toggle('light-mode');
+});
