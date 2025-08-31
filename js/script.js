d7930bf508a8cd233353787cca19265accf7a3ce
Niki Yaw <ql231995@student.ubc.ca>
2025-08-31 11:48:49 -0700

    fix typo in js file

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
 // Add an event listener for clicks on the button
-themeToggle.addEvent<IntersectionObserverEntry('click', function() {
+themeToggle.addEventLIstener('click', function() {
     // Toggle the 'light-mode' class on the body
     body.classList.toggle('light-mode');
 });
