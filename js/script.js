77b47d6d519441f25b7e692ad3f3ab7db9c0e1f0
Niki Yaw <nikyaw95@gmail.com>
2025-11-08 15:39:19 -0800

    remove lightbulb & change page layout

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Get the button and the body element
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-
-// Add an event listener for clicks on the button
-themeToggle.addEventListener('click', function() {
-    // Toggle the 'light-mode' class on the body
-    body.classList.toggle('light-mode');
-});
